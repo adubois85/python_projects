02b22721d7bf5ff6064d5e5fd10d6f40fcb63d72
Alex DuBois <adubois@alumni.uci.edu>
2018-11-04 17:54:04 -0700

    Transformed the phrase in panic.py in the most convoluted way possible

--- head_first/chapter2/panic.py
+++ head_first/chapter2/panic.py
@@ -2,8 +2,16 @@ phrase = "Don't panic!"
 plist = list(phrase)
 print(phrase)
 print(plist)
+# Use list manipulation methods so the final 2 print functions display "on tap"
 
-
+for i in range(4):
+    plist.pop()
+plist.pop(0)
+plist.pop(2)
+temp = plist.pop(3)
+plist.insert(2, temp)
+temp = plist.pop()
+plist.insert((len(plist) -1), temp)
 
 
 new_phrase = ''.join(plist)
