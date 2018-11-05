76147d5192cd497a0993eeaad586568b76281a86
Alex DuBois <adubois@alumni.uci.edu>
2018-11-04 17:58:11 -0700

    Added book's solution for reference

--- head_first/chapter2/panic.py
+++ head_first/chapter2/panic.py
@@ -13,6 +13,14 @@ plist.insert(2, temp)
 temp = plist.pop()
 plist.insert((len(plist) -1), temp)
 
+# How the book does it
+
+# for i in range(4):
+#     plist.pop()
+# plist.pop(0)
+# plist.remove("'")
+# plist.extend([plist.pop(), plist.pop()])
+# plist.insert(2, plist.pop(3))
 
 new_phrase = ''.join(plist)
 print(plist)
