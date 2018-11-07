857eb868c457d86cb49af9599182fbeebdb9f077
Alex DuBois <adubois@alumni.uci.edu>
2018-11-06 17:45:20 -0700

    Set up panic2.py for manipulating with slice notation

---
+++ head_first/chapter2/panic2.py
@@ -0,0 +1,11 @@
+phrase = "Don't panic!"
+plist = list(phrase)
+print(phrase)
+print(plist)
+
+#Use slice notation to transform the initial phrase to the phrase "on tap"
+
+
+
+print(plist)
+print(new_phrase)
