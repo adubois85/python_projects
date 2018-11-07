db6370b6d839c125927ea9f1f758ccbe4700a406
Alex DuBois <adubois@alumni.uci.edu>
2018-11-06 17:56:58 -0700

    Manipulated phrase into target in panic2.py using slice notation

--- head_first/chapter2/panic2.py
+++ head_first/chapter2/panic2.py
@@ -4,8 +4,10 @@ print(phrase)
 print(plist)
 
 #Use slice notation to transform the initial phrase to the phrase "on tap"
-
-
+new_phrase = plist[1:3]
+new_phrase.extend(plist[5:3:-1])
+new_phrase.extend(plist[-5: -7: -1])
+new_phrase = ''.join(new_phrase)
 
 print(plist)
 print(new_phrase)
