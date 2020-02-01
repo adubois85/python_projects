a05b171d7ba1faa9f6dfa9ef6fb3f7dbe7d5b7ec
Alex DuBois <adubois@alumni.uci.edu>
2020-01-31 17:40:52 -0700

    rearranging things to better track the book

--- head_first/chapter2/vowels.py
+++ head_first/chapter2/vowels.py
@@ -1,9 +1,6 @@
 vowels = ['a', 'e', 'i', 'o', 'u']
-found = []
-word = input('Type your word here to search for the vowels: ')
+word = "Milliways"
 
 for letter in word:
-    if letter in vowels and letter not in found:
-        found.append(letter)
-for vowel in found:
-    print(vowel)
+    if letter in vowels:
+        print(letter)
