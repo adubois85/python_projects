752b43220885a1bd8bc468d72e486eef10f556a3
Alex DuBois <adubois@alumni.uci.edu>
2020-02-03 16:47:23 -0700

    Vowel7, now featuring sets!

--- head_first/chapter2/vowels7.py
+++ head_first/chapter2/vowels7.py
@@ -1,9 +1,10 @@
-vowels = ['a', 'e', 'i', 'o', 'u']
-found = []
+vowels = set('aeiou')
 word = input('Type your word here to search for the vowels: ')
 
-for letter in word:
-    if letter in vowels and letter not in found:
-        found.append(letter)
-for vowel in found:
-    print(vowel)
+print(vowels.intersection(set(word)))
+
+# for letter in word:
+#     if letter in vowels and letter not in found:
+#         found.append(letter)
+# for vowel in found:
+#     print(vowel)
