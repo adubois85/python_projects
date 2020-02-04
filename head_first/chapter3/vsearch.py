c51ca0568c4f151af083822392d24e5db37d4b33
Alex DuBois <adubois@alumni.uci.edu>
2020-02-03 18:10:00 -0700

    changed vowel search back to my style

--- head_first/chapter3/vsearch.py
+++ head_first/chapter3/vsearch.py
@@ -2,9 +2,11 @@ def search_for_vowels():
     """Propmpts user for a word and returns any vowels in it (but not 'y')"""
     vowels = set('aeiou')
     word = input('Type your word here to search for the vowels: ')
-    found = vowels.intersection(set(word))
-    for vowel in found:
-        print(vowel)
+    print(vowels.intersection(set(word)))
+
+    # found = vowels.intersection(set(word))
+    # for vowel in found:
+    #     print(vowel)
 
 
 search_for_vowels()
