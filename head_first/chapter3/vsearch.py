62afd32ccb0789afcbc1ff21f30ee696331a3dd9
Alex DuBois <adubois@alumni.uci.edu>
2020-02-03 17:45:34 -0700

    created vsearch.py, copied mostly from vowels7

---
+++ head_first/chapter3/vsearch.py
@@ -0,0 +1,6 @@
+def search_for_vowels():
+    vowels = set('aeiou')
+    word = input('Type your word here to search for the vowels: ')
+    found = vowels.intersection(set(word))
+    for vowel in found:
+        print(vowel)
