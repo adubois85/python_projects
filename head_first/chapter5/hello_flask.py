9bae221614b305cbf4a7f86cc76a7cfcda99b593
Alex DuBois <adubois@alumni.uci.edu>
2020-02-10 18:28:18 -0700

    added letter search to simple flask app

--- head_first/chapter5/hello_flask.py
+++ head_first/chapter5/hello_flask.py
@@ -8,4 +8,14 @@ def hello() -> str:
     return 'Hello world from Flask!'
 
 
+@app.route('/search4')
+def do_search() -> str:
+    return str(search4letters('life, the universe, and everything', 'eiru,!'))
+
+
+def search4letters(phrase: str, letters: str = 'aeiou') -> set:
+    """Returns set of 'letters' found in 'phrase'."""
+    return set(letters).intersection(set(phrase))
+
+
 app.run()
