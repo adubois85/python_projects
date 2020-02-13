2a79378f21553c999f9c7ba5c99ca24994dec2c8
Alex DuBois <adubois@alumni.uci.edu>
2020-02-13 15:31:17 -0700

    I need to explicitly allow both GET and POST?

--- head_first/chapter5/hello_flask.py
+++ head_first/chapter5/hello_flask.py
@@ -8,7 +8,7 @@ def hello() -> str:
     return 'Hello world from Flask!'
 
 
-@app.route('/search4')
+@app.route('/search4', methods=['GET', 'POST'])
 def do_search() -> str:
     return str(search4letters('life, the universe, and everything', 'eiru,!'))
 
@@ -18,7 +18,7 @@ def search4letters(phrase: str, letters: str = 'aeiou') -> set:
     return set(letters).intersection(set(phrase))
 
 
-@app.route('/entry')
+@app.route('/entry', methods=['GET', 'POST'])
 def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the web!')
