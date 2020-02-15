22965c5c5b06fb0bd6197835ce23424173a6696d
Alex DuBois <adubois@alumni.uci.edu>
2020-02-14 18:33:33 -0700

    app.run will now only run when not a module

--- head_first/chapter5/hello_flask.py
+++ head_first/chapter5/hello_flask.py
@@ -34,4 +34,5 @@ def entry_page() -> 'html':  # noqa: F821
                            the_title='Welcome to search4letters on the web!')
 
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
