3892865896db13bf6d4e81889324a2651b1ae557
Alex DuBois <adubois@alumni.uci.edu>
2020-02-15 16:54:40 -0700

    modified request log to save relevant data

--- head_first/chapter5/hello_flask.py
+++ head_first/chapter5/hello_flask.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:  # noqa: F821
     with open('vsearch.log', 'a') as log:
-        print(req, res, file=log)
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 
 # @app.route('/')
