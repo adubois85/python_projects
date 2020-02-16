b592401de9477972bd03ba5f84696c092235d70c
Alex DuBois <adubois@alumni.uci.edu>
2020-02-15 18:39:00 -0700

    copied changes to view_log in chapter 5

--- head_first/chapter5/hello_flask.py
+++ head_first/chapter5/hello_flask.py
@@ -52,7 +52,11 @@ def view_log() -> str:
             contents.append([])
             for item in line.split('|'):
                 contents[-1].append(escape(item))
-    return str(contents)
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                           the_title='View log',
+                           the_row_titles=titles,
+                           the_data=contents)
 
 
 if __name__ == '__main__':
