dee33deec9f8e0c761253ff3014607b03f141468
Alex DuBois <adubois@alumni.uci.edu>
2020-02-17 14:42:58 -0700

    added comments to suppress errors

--- head_first/chapter5/hello_flask.py
+++ head_first/chapter5/hello_flask.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:  # noqa: F821
     with open('vsearch.log', 'a') as log:
-        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')  # noqa: E501
 
 
 # @app.route('/')
