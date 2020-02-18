c776847b7b4e08e61b6c1913247446fb5049ec1e
Alex DuBois <adubois@alumni.uci.edu>
2020-02-18 16:37:41 -0700

    added method for representing object as a string

--- head_first/chapter8/countfromby.py
+++ head_first/chapter8/countfromby.py
@@ -4,5 +4,9 @@ class CountFromBy:
         self.val = v
         self.incr = i
 
+    def __repr__(self) -> str:
+        return str(self.val)
+
     def increase(self) -> None:
         self.val += self.incr
+
