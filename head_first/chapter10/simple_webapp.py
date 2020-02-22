9306f5162e1ab27814a670c9af5746114464e4ce
Alex DuBois <adubois@alumni.uci.edu>
2020-02-21 18:51:10 -0700

    added decorators to the test page routes

--- head_first/chapter10/simple_webapp.py
+++ head_first/chapter10/simple_webapp.py
@@ -13,11 +13,11 @@ def do_logout() -> str:
     session.pop('logged_in')
     return 'You are now logged out.'
 
-@app.route('/status')
-def check_status() -> str:
-    if 'logged_in' in session:
-        return 'You are currently logged in.'
-    return 'You are NOT logged in.'
+# @app.route('/status')
+# def check_status() -> str:
+#     if 'logged_in' in session:
+#         return 'You are currently logged in.'
+#     return 'You are NOT logged in.'
 
 app.secret_key = 'MyKeyIsSecretYo'
 
@@ -26,14 +26,17 @@ def hello() -> str:
     return 'Hello from the simple webapp'
 
 @app.route('/page1')
+@check_logged_in
 def page1() -> str:
     return 'This is page 1.'
 
 @app.route('/page1')
+@check_logged_in
 def page2() -> str:
     return 'This is page 2.'
 
 @app.route('/page1')
+@check_logged_in
 def page3() -> str:
     return 'This is page 3.'
 
