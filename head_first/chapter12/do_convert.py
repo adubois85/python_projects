d586f0b8a92f630e67255e14c6bb7d10ec45751b
Alex DuBois <adubois@alumni.uci.edu>
2020-05-15 16:30:14 -0600

    added function to read, copy csv to new dict

--- head_first/chapter12/do_convert.py
+++ head_first/chapter12/do_convert.py
@@ -4,3 +4,11 @@ from pprint import pprint
 
 def convert12hour(time24:str) -> str:
     return datetime.strptime(time24, '%H:%M').strftime('%I:%M%p')
+
+with open('schedule.csv') as data:
+    ignore = data.readline()
+    flights = {}
+    for line in data:
+        k, v = line.strip().split(',')
+        flights[k] = v
+
