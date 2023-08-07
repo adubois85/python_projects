be0b77b09bfe4d079d24c168afd0c65734ed6f1e
Alex DuBois <adubois@alumni.uci.edu>
2023-08-07 15:49:38 -0600

    fixed find_palingrams fn for forward case

--- impractical_python/chapter_02/palingrams.py
+++ impractical_python/chapter_02/palingrams.py
@@ -5,9 +5,11 @@ def find_palingrams():
     words = load_dictionary.load("words.txt")
     palingrams = []
     for word in words:
+        end = len(word)
         rev_word = word[::-1]
-        if len(word) > 1:
-            for i in range(len(word)):
-                if rev_word[i:] in words:
-                    palingrams.append(word + word[::-i])
+        if end > 1:
+            # I misunderstood the assignment and was making lots of unnnecessary comparisons
+            for i in range(end):
+                if word[i:] == rev_word[:end - i] and rev_word[end - i:] in words:
+                    palingrams.append(f"{word} {rev_word[end - i:]}")
     return palingrams
