4c34b31d34e9996839ee411f8775f59aedbfa17e
Alex DuBois <adubois@alumni.uci.edu>
2023-08-08 15:57:38 -0600

    words aren't anagrams of themselves

--- impractical_python/chapter_03/anagrams.py
+++ impractical_python/chapter_03/anagrams.py
@@ -4,10 +4,11 @@ import load_dictionary
 def find_anagrams():
     anagrams = []
     words = load_dictionary.load("../chapter_02/words.txt")
-    user_word = input("Enter a single word to see its anagrams.\n")
-    user_word = sorted(user_word.lower())
+    user_word = input("Enter a single word to see its anagrams.\n").lower()
+    user_word_sorted = sorted(user_word)
     for word in words:
-        sorted_word = sorted(word)
-        if sorted_word == user_word:
-            anagrams.append(word)
+        if word != user_word:
+            sorted_word = sorted(word)
+            if sorted_word == user_word_sorted:
+                anagrams.append(word)
     return anagrams
