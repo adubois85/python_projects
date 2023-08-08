c4ca76913fc2733985e6f77ff1f3e5c23f56e0ec
Alex DuBois <adubois@alumni.uci.edu>
2023-08-08 16:32:48 -0600

    created skeleton of anagram phrase finder

---
+++ impractical_python/chapter_03/anagrams_phrase.py
@@ -0,0 +1,12 @@
+'''Progressively builds an anagram from user-inputted name'''
+from collections import Counter
+import load_dictionary
+
+def main():
+    pass
+
+def find_anagrams():
+    pass
+
+def process_choice():
+    pass
