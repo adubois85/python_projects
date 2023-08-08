bacc943630975441e3cfdac9cbbd084b557624bd
Alex DuBois <adubois@alumni.uci.edu>
2023-08-08 17:05:44 -0600

    request name + strip of non-alphas, load word list

--- impractical_python/chapter_03/anagrams_phrase.py
+++ impractical_python/chapter_03/anagrams_phrase.py
@@ -1,6 +1,13 @@
 '''Progressively builds an anagram from user-inputted name'''
 from collections import Counter
 import load_dictionary
+import re
+
+# This should strip everything that isn't one of the 26 letters from the name
+name = input("Enter a full name and get to building an anagram of it.\n")
+name = re.findall(r'[a-zA-Z]+', name.lower())
+name = Counter("".join(name))
+words = load_dictionary.load("../chapter_02/words.txt")
 
 def main():
     pass
