2b8ef3d82d3cfacc51e5e85f8695bff691bf1e9f
Alex DuBois <adubois@alumni.uci.edu>
2023-08-09 16:14:57 -0600

    filling out the main fn of phrase finder

--- impractical_python/chapter_03/anagrams_phrase.py
+++ impractical_python/chapter_03/anagrams_phrase.py
@@ -1,6 +1,7 @@
 '''Progressively builds an anagram from user-inputted name'''
 from collections import Counter
 import load_dictionary
+import pprint as pp
 import re
 
 # This should strip everything that isn't one of the 26 letters from the name
@@ -10,7 +11,14 @@ name = Counter("".join(name))
 words = load_dictionary.load("../chapter_02/words.txt")
 
 def main():
-    pass
+    name_limit = len(name)
+    anagram_phrase = []
+    phrase_limit = sum(map(len, anagram_phrase))
+    while phrase_limit < name_limit:
+        possible_anagrams = find_anagrams(name, words)
+        pp.pprint(possible_anagrams)
+        print(f'The current anagram is "{" ".join(anagram_phrase)}"')
+
 
 def find_anagrams(name, words: list) -> list:
     name = Counter(name)
